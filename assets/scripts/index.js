4fb24b460b97cfd7623e57aa9b6085ae3eb65d3d
enowjohn <148995120+enowjohn@users.noreply.github.com>
2024-02-08 15:49:54 +0100

    expected indentation in js file corrected

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,32 +1,32 @@
 
 function palindrome(str) {
- const result = document.querySelectorAll('#result')
+  const result = document.querySelectorAll('#result')
 
- const array = str.split(' ').join('')
- let j = array.length - 1
- for (let i = 0; i <= j / 2; i++) {
-  if (array[i] !== array[j]) {
-   return false
+  const array = str.split(' ').join('')
+  let j = array.length - 1
+  for (let i = 0; i <= j / 2; i++) {
+    if (array[i] !== array[j]) {
+      return false
+    }
+    j--
   }
-  j--
- }
- return true
+  return true
 }
 
 console.log(palindrome('do gees see god'))
 
 function checkPalindrome() {
- const word = document.getElementById('question').value
- const palindrom = palindrome(word)
- const answer = document.getElementById('result')
+  const word = document.getElementById('question').value
+  const palindrom = palindrome(word)
+  const answer = document.getElementById('result')
 
- if (palindrom) {
-  answer.innerHTML = 'The word entered is a Palindrome'
- } else {
-  answer.innerHTML = 'The word entered is not a Palindrome'
- }
+  if (palindrom) {
+    answer.innerHTML = 'The word entered is a Palindrome'
+  } else {
+    answer.innerHTML = 'The word entered is not a Palindrome'
+  }
 }
 
 if (onclick) {
- checkPalindrome()
+  checkPalindrome()
 }
